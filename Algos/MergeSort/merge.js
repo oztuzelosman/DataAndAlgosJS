f330593e0292fb0ccc02f6fd7fb3f01959e95636
oztuzelosman <oztuzelosman@gmail.com>
2022-08-15 10:29:17 +0300

    learn merge sort and apply it on js

---
+++ Algos/MergeSort/merge.js
@@ -0,0 +1,28 @@
+function mergeSort(arr) {
+  if (arr.length < 2) {
+    return arr;
+  }
+
+  const mid = Math.floor(arr.length / 2);
+  const leftArr = arr.slice(0, mid);
+  const rightArr = arr.slice(mid);
+
+  return merge(mergeSort(leftArr), mergeSort(rightArr));
+}
+
+function merge(leftArr, rightArr) {
+  console.log(leftArr, rightArr);
+  const sortedArr = [];
+
+  while (leftArr.length && rightArr.length) {
+    if (leftArr[0] < rightArr[0]) {
+      sortedArr.push(leftArr.shift());
+    } else {
+      sortedArr.push(rightArr.shift());
+    }
+  }
+  return [...sortedArr, ...leftArr, ...rightArr];
+}
+
+const unsortedArr = [-2, -7, 1, 9, 8];
+console.log(mergeSort(unsortedArr));
